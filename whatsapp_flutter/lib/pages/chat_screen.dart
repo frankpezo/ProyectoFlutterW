6d9512d67a35ede56853a775a79f939644de3edc
Frank Pezo <frankanderpg.2000@gmail.com>
2021-03-01 18:27:51 -0500

    Agregamos el mensaje y el chat

--- whatsapp_flutter/lib/pages/chat_screen.dart
+++ whatsapp_flutter/lib/pages/chat_screen.dart
@@ -9,10 +9,53 @@ class ChatScreen extends StatefulWidget{
 }
 
 class _ChatScreenState extends State<ChatScreen>{
+  final TextEditingController _textController = new TextEditingController();
+   
+   void _handledSubmit(String text){
+      print(text);
+   }
+
+  // Declaramos aquí la función
+  Widget  _builTextComposer(){
+    //Color para los iconos
+      return new IconTheme(
+        data: new IconThemeData(color: Theme.of(context).primaryColor),
+          child:  new Container(
+           //Creamos el input
+           child: new Row(
+             children: <Widget>[
+                new Flexible(
+                  //Colocamos el input
+                child:new TextField(
+                   controller: _textController,
+                )
+                ), 
+                new Container(
+                  child: new IconButton(
+                     icon: new Icon(Icons.send),
+                     onPressed: ()=>_handledSubmit(_textController.text),
+                  ),
+                )
+             ],
+           ),
+       ));
+  }
     @override
     Widget build(BuildContext context) {
-      return new Container(
-        child: new Text(widget.name),
-      ) ;
+       return new Scaffold(
+          appBar:   new AppBar(
+        title: new Text(widget.name),//EL nombre de la persona al que le mandaremos el mensaje
+          ), 
+        body: new Container(
+           //Realizamos las modificaciones
+           child: new Column(
+             children: <Widget>[
+                 new Container(
+                   child: _builTextComposer(),//Creamos la función
+                 )
+             ],
+           ),
+        ),
+       );
     }
 }
